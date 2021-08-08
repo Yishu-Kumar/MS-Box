b21c34e74f0e417b94d9b4d6b1c89c53f8ba866d
Yishu Kumar <86670360+Yishu-Kumar@users.noreply.github.com>
2021-08-08 19:11:02 +0530

    Update EmailService.java

--- EmailAPI/src/main/java/org/springboot/emailapi/service/EmailService.java
+++ EmailAPI/src/main/java/org/springboot/emailapi/service/EmailService.java
@@ -20,47 +20,36 @@ public class EmailService {
 
         //Get the system properties.
         Properties properties = System.getProperties();
-        System.out.println("Properties: " + properties);
-
-        //Setting the important information to properties object.
-        properties.put("mail.smtp.host", host); //for setting "Host".
-        properties.put("mail.smtp.port", "465"); //587.
-        properties.put("mail.smtp.ssl.enable", "true"); //"SSL" -> "Secure Socket Layer", for security purpose.
+        
+        properties.put("mail.smtp.host", host); 
+        properties.put("mail.smtp.port", "465"); 
+        properties.put("mail.smtp.ssl.enable", "true"); 
         properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.auth", "true"); //for enabling "Authentication".
+        properties.put("mail.smtp.auth", "true"); 
 
-        //Step 1. Get the "Session" object.
         Session session = Session.getInstance(properties, new Authenticator() {
 
             @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-
-                if (login.getEmail().equals("goyalyishu94174@gmail.com")) {
-                    return new PasswordAuthentication(login.getEmail(), "uwwu gxej orvw qmit"); //From which we want send email.
-                }
-
+            protected PasswordAuthentication getPasswordAuthentication() 
+                
                 return new PasswordAuthentication(login.getEmail(), login.getPassword()); //From which we want send email.
             }
         });
-//        ygAx-K79$wC7.#b
-        session.setDebug(true); //To get debug message for error.
-
-        //Step 2. Compose the message (text, multi media).
+        
+        session.setDebug(true); 
+        
         MimeMessage mimeMessage = new MimeMessage(session);
 
         try {
 
-            mimeMessage.setFrom(login.getEmail()); //Set "From".
-            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(to)); //Adding "Recipient" to message. We can pass th array of "Addresses" like "InternetAddress()".
-            mimeMessage.setSubject(subject); //Set the message subject.
-            mimeMessage.setText(message); //Set the message.
+            mimeMessage.setFrom(login.getEmail()); 
+            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(to)); 
+            mimeMessage.setSubject(subject);
+            mimeMessage.setText(message);
 
-            //Step 3. Send the message using "Transport" class.
             Transport.send(mimeMessage);
 
             status = true;
-
-            System.out.println("Sent message...........................");
 
         } catch (MessagingException e) {
 
